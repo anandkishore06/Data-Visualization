b0b47c653b0220b3c3e8059c262c78980f57054d
Kishore <Anand.Kishore@lims.com>
2024-02-25 10:58:10 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import UserTable from './Components/UserTable/UserTable';
 import './App.css';
+import React from 'react';
+// import Charts from './Charts/Charts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <UserTable />
+      {/* <Charts /> */}
     </div>
   );
 }
